d28e4ee628d20567fe050398303ab1a081dfb9ef
lnshi <lnshi@jinhetech.com>
2024-11-13 17:02:03 +0800

    feature:集成craco，对webpack配置进行覆盖

---
+++ craco.config.ts
@@ -0,0 +1,60 @@
+import path from 'path';
+import { name } from './package.json';
+import cracoLess from 'craco-less';
+
+const pathResolve = (pathUrl: string) => path.join(__dirname, pathUrl);
+
+module.exports = {
+  reactScriptsVersion: 'react-scripts' /* (default value) */,
+  plugins: [
+    {
+      plugin: cracoLess,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: {
+              "@primary-color": "#1890ff",
+            },
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    alias: {
+      '@': pathResolve('src'),
+      '@assets': pathResolve('src/assets'),
+      '@components': pathResolve('src/components'),
+      '@constants': pathResolve('src/constants'),
+      '@containers': pathResolve('src/containers'),
+      '@hooks': pathResolve('src/hooks'),
+      '@mocks': pathResolve('src/mocks'),
+      '@routes': pathResolve('src/routes'),
+      '@services': pathResolve('src/services'),
+      '@styles': pathResolve('src/styles'),
+      '@types': pathResolve('src/types'),
+      '@utils': pathResolve('src/utils'),
+      '@contexts': pathResolve('src/contexts'),
+    },
+    configure(webpackConfig: any) {
+      // 配置扩展扩展名
+      webpackConfig.resolve.extensions = [...webpackConfig.resolve.extensions, ...['.scss', '.css']];
+      // 接入微前端框架qiankun的配置,不接入微前端可以不需要
+      webpackConfig.output.library = `${name}-[name]`;
+      webpackConfig.output.libraryTarget = 'umd';
+      webpackConfig.output.globalObject = 'window';
+      return webpackConfig;
+    },
+  },
+  devServer: {
+    // 本地服务的端口号
+    port: 3001,
+    // 本地服务的响应头设置
+    headers: {
+      // 允许跨域
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+};
+
